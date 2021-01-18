c2c52b8db36ad3fb3ada6934b2557cb2cfb43c16
LeANHTuan59 <tenlatuan1002@gmail.com>
2021-01-18 22:35:19 +0700

    LTQL

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Wolrd!");
+            Console.Write("Ten dang nhap: ");
+            Console.WriteLine("Chao mung " + Console.ReadLine() + " den voi mon hoc LTQL");
+        }
+    }
+}
+
+
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giaiptb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x;
+            Console.WriteLine("Nhap he so a:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap he so b:");
+            float b = float.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                if (b == 0)
+                    Console.WriteLine("Phuong trinh vo so nghiem");
+                else
+                    Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else
+            {
+                x = -b / a;
+                Console.WriteLine("Phuong trinh co nghiem = " + x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
